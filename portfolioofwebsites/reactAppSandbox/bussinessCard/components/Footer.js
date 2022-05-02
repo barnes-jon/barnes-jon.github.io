e41f5e8d6e69f742b0ffe0ca89d07694b68c8d54
Jon Barnes <103963671+barnes-jon@users.noreply.github.com>
2022-05-01 20:52:25 -0700

    Create Footer.js

---
+++ portfolioofwebsites/reactAppSandbox/bussinessCard/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer(){
+    return(
+        <footer>
+            <img src="" alt="linkdin logo" />
+            <img src="" alt="github logo" />
+            <img src="" alt="twitter logo" />
+        </footer>
+    )
+}
+
+export default Footer
