c25451e3d246fb6e90857d9cfa42a9b242ddd9c0
Jon Barnes <103963671+barnes-jon@users.noreply.github.com>
2022-05-01 20:52:02 -0700

    Create AboutMe.js

---
+++ portfolioofwebsites/reactAppSandbox/bussinessCard/components/AboutMe.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function AboutMe(){
+    return(
+        <>
+        <h2 className="aboutMe--title">About</h2>
+        <p>I am learning React</p>
+        </>
+    )
+}
+
+export default AboutMe
