98899cfae20f4d5c7772efe20d8bd5089c7a5e71
Jon Barnes <103963671+barnes-jon@users.noreply.github.com>
2022-05-01 20:53:22 -0700

    Create Interests.js

---
+++ portfolioofwebsites/reactAppSandbox/bussinessCard/components/Interests.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Interests(){
+    return(
+        <>
+        <h2 className="interests--title">Interests</h2>
+        <p>I like geology, coding, and iced tea.  
+        I like the ESO32 and IoT things.</p>
+        </>
+    )
+}
+
+export default Interests
