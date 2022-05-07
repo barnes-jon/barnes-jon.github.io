579eeb0d46ae7505a5cdb9f74a025d7e1a57ce7a
Jon Barnes <103963671+barnes-jon@users.noreply.github.com>
2022-05-06 21:09:31 -0700

    Update Hero.js

--- portfolioofwebsites/reactAppSandbox/experience/components/Hero.js
+++ portfolioofwebsites/reactAppSandbox/experience/components/Hero.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Hero(){
     return(
         <div className = "hero">
-            <img src="../images/photo-grid.png" className="hero--photogrid"/>
+            <img src="../images/Unknown.jpeg" className="hero--photogrid"/>
             <h1>Online Experiences</h1>
             <h2>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</h2>
         </div>
