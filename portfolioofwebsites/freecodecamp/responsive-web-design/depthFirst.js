4caa6b2ee1fea8653569936b5aa0df5cc7b4c493
Jon Barnes <103963671+barnes-jon@users.noreply.github.com>
2022-05-22 23:31:02 -0700

    Create depthFirst.js

---
+++ portfolioofwebsites/freecodecamp/responsive-web-design/depthFirst.js
@@ -0,0 +1,127 @@
+class Graph {
+  constructor(isDirected = false, isWeighted = false) {
+    this.vertices = [];
+    this.isDirected = isDirected;
+    this.isWeighted = isWeighted;
+  }
+
+  addVertex(data) {
+    const newVertex = new Vertex(data);
+    this.vertices.push(newVertex);
+    return newVertex;
+  }
+
+  removeVertex(vertex) {
+    this.vertices = this.vertices.filter(v => v !== vertex);
+  }
+
+  addEdge(vertexOne, vertexTwo, weight) {
+    const edgeWeight = this.isWeighted ? weight : null;
+
+    if (vertexOne instanceof Vertex && vertexTwo instanceof Vertex) {
+      vertexOne.addEdge(vertexTwo, edgeWeight);
+      if (!this.isDirected) {
+        vertexTwo.addEdge(vertexOne, edgeWeight);
+      }
+    } else {
+      throw new Error('Expected Vertex arguments.');
+    }
+  }
+
+  removeEdge(vertexOne, vertexTwo) {
+    if (vertexOne instanceof Vertex && vertexTwo instanceof Vertex) {
+      vertexOne.removeEdge(vertexTwo);
+      if (!this.isDirected) {
+        vertexTwo.removeEdge(vertexOne);
+      }
+    } else {
+      throw new Error('Expected Vertex arguments.');
+    }
+  }
+
+  getVertexByValue(value) {
+    return this.vertices.find(vertex => vertex.data === value);
+  }
+
+  print() {
+    const vertexList = this.vertices;
+    vertexList.forEach(vertex => vertex.print());
+  }
+}
+
+class Vertex {
+  constructor(data) {
+    this.data = data;
+    this.edges = [];
+  }
+
+  addEdge(vertex, weight) {
+    if (vertex instanceof Vertex) {
+      this.edges.push(new Edge(this, vertex, weight));
+    } else {
+      throw new Error('Edge start and end must both be Vertex');
+    }
+  }
+
+  removeEdge(vertex) {
+    this.edges = this.edges.filter(edge => edge.end !== vertex);
+  }
+
+  print() {
+    const edgeList = this.edges.map(edge =>
+        edge.weight !== null ? `${edge.end.data} (${edge.weight})` : edge.end.data) || [];
+
+    const output = `${this.data} --> ${edgeList.join(', ')}`;
+    console.log(output);
+  }
+}
+
+class Edge {
+  constructor(start, end, weight = null) {
+    this.start = start;
+    this.end = end;
+    this.weight = weight;
+  }
+}
+
+const simpleGraph = new Graph(true, false);
+const startNode = simpleGraph.addVertex('v0.0.0');
+const v1 = simpleGraph.addVertex('v1.0.0');
+const v2 = simpleGraph.addVertex('v2.0.0');
+
+const v11 = simpleGraph.addVertex('v1.1.0');
+const v12 = simpleGraph.addVertex('v1.2.0');
+const v21 = simpleGraph.addVertex('v2.1.0');
+
+const v111 = simpleGraph.addVertex('v1.1.1');
+const v112 = simpleGraph.addVertex('v1.1.2');
+const v121 = simpleGraph.addVertex('v1.2.1');
+const v211 = simpleGraph.addVertex('v2.1.1');
+
+simpleGraph.addEdge(startNode, v1);
+simpleGraph.addEdge(startNode, v2);
+
+simpleGraph.addEdge(v1, v11);
+simpleGraph.addEdge(v1, v12);
+simpleGraph.addEdge(v2, v21);
+
+simpleGraph.addEdge(v11, v111);
+simpleGraph.addEdge(v11, v112);
+simpleGraph.addEdge(v12, v121);
+simpleGraph.addEdge(v21, v211);
+
+
+const depthFirstTraversal = (start, visitedVertices = [start]) => {
+  console.log(start.data);
+
+ start.edges.forEach(edge => {
+    const neighbor = edge.end;
+
+    if (!visitedVertices.includes(neighbor)) {
+      visitedVertices.push(neighbor);
+      depthFirstTraversal(neighbor, visitedVertices);
+    }
+ });
+};
+
+depthFirstTraversal(testGraph.vertices[0]);
