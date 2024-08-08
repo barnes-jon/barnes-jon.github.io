cf5e526bb5b25eebf4630df8d860845bc3c34904
Jon Barnes <103963671+barnes-jon@users.noreply.github.com>
2024-08-08 03:17:07 -0700

    Update scripts.js

--- maps/geology/new_mexico/four_maps/scripts.js
+++ maps/geology/new_mexico/four_maps/scripts.js
@@ -66,7 +66,7 @@ fetch('soils_silver_city_deming_t_or_c.geojson')
                 };
             },
             onEachFeature: function(feature, layer) {
-                layer.bindPopup(feature.properties.mukey);
+                layer.bindPopup(feature.properties.MUKEY);
             }
         }).addTo(map3);
     })
